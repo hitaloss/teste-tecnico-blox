180415e0b43f7b2e77e1f18b584c8d67fb23452d
hitalo <hitalo.2014.hs@gmail.com>
2023-02-28 11:09:19 -0300

    feat: base model for accounts app

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from djmoney.models.fields import MoneyField
+from django.contrib.auth.models import AbstractUser
+
+class Account(AbstractUser):
+    id = models.IntegerField(primary_key=True, editable=False)
+    id_pessoa = models.IntegerField(unique=True, editable=False)
+    saldo = MoneyField(max_digits=15, decimal_places=2)
+    limite_saque_diario = MoneyField(max_digits=15, decimal_places=2)
+    flag_ativo = models.BooleanField(default=True)
+    tipo_conta = models.IntegerField(default=1)
+    data_criacao = models.DateTimeField(auto_now_add=True)
