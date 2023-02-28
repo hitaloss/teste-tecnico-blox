df9bf90819cae12fc4a7c7c8f78773bce29c5057
hitalo <hitalo.2014.hs@gmail.com>
2023-02-28 15:42:14 -0300

    fix: removed swagger-ui url from accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from drf_spectacular.views import SpectacularSwaggerView
 from . import views
 
 
 urlpatterns = [
     path("account/", views.AccountCreateView.as_view()),
     path("account/<int:pk>/balance/", views.AccountBalanceDetailView.as_view()),
-    path("account/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
