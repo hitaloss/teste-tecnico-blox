2194eef0dcbe0615aafbd23ed07d81a698712530
hitalo <hitalo.2014.hs@gmail.com>
2023-02-28 14:01:16 -0300

    feat: account balance request basic view structure made

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,14 @@ from .models import Account
 from .serializers import AccountsSerializer
 
 
-class AccountCreate(generics.CreateAPIView):
+class AccountCreateView(generics.CreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountsSerializer
-    
+
+class AccountBalanceDetailView(generics.RetrieveAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountsSerializer
+
+    def get_object(self):
+        account_id = self.kwargs.get("pk")
+        return Account.objects.filter(pk=account_id).values("saldo").first()
