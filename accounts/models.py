d513c23ece4f59e7f359eaa61e4f1198543f60d0
hitalo <hitalo.2014.hs@gmail.com>
2023-03-01 10:18:11 -0300

    fix: money validations and account id fixed

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
-from django.contrib.auth.models import AbstractUser
+from djmoney.models.validators import MinMoneyValidator, MaxMoneyValidator
+
 
 class Account(models.Model):
-    id = models.IntegerField(primary_key=True, editable=False)
-    id_pessoa = models.IntegerField(unique=True, editable=False)
-    saldo = MoneyField(max_digits=15, decimal_places=2)
-    limite_saque_diario = MoneyField(max_digits=15, decimal_places=2)
+    id = models.BigAutoField(primary_key=True, editable=False)
+    id_pessoa = models.PositiveIntegerField(unique=True)
+    saldo = MoneyField(
+        max_digits=15,
+        decimal_places=2,
+        default_currency="BRL",
+        validators=[
+            MinMoneyValidator(0),
+            MaxMoneyValidator(999999999999999),
+        ],
+    )
+    limite_saque_diario = MoneyField(
+        max_digits=15,
+        decimal_places=2,
+        default_currency="BRL",
+        validators=[
+            MinMoneyValidator(0),
+            MaxMoneyValidator(3000),
+        ],
+    )
     flag_ativo = models.BooleanField(default=True)
     tipo_conta = models.IntegerField(default=1)
     data_criacao = models.DateTimeField(auto_now_add=True)
