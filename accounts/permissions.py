510335e2a857af09644b10974c20444830b028d1
hitalo <hitalo.2014.hs@gmail.com>
2023-03-01 12:30:36 -0300

    fix: added custom message, for better understanding

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,8 +1,9 @@
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 
 class AccountDeactivated(permissions.BasePermission):
     def has_permission(self, request, view):
         if view.get_object().flag_ativo:
             return True
-        return False
+        raise PermissionDenied("You account must be active for this request.")
