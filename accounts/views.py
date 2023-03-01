72dee05de11f173cf861c026ae2def5f1778a8ca
hitalo <hitalo.2014.hs@gmail.com>
2023-03-01 10:24:32 -0300

    fix: fixed method used to bring the balance of any account by id

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,23 @@
+import ipdb
 from rest_framework import generics
 from .models import Account
 from .serializers import AccountsSerializer
+from .serializers import AccountsDetailSerializer
+from django.http import Http404
 
 
 class AccountCreateView(generics.CreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountsSerializer
 
+
 class AccountBalanceDetailView(generics.RetrieveAPIView):
     queryset = Account.objects.all()
-    serializer_class = AccountsSerializer
+    serializer_class = AccountsDetailSerializer
 
     def get_object(self):
         account_id = self.kwargs.get("pk")
-        return Account.objects.filter(pk=account_id).values("saldo").first()
+        account = Account.objects.filter(pk=account_id).first()
+        if not account:
+            raise Http404
+        return account
