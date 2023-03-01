41652575256cf8a7dcf23008cc37f39d2f2e23f4
hitalo <hitalo.2014.hs@gmail.com>
2023-03-01 10:23:42 -0300

    fix: fixed some validating settings plus added another serializer for balance searching

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,17 +1,38 @@
 from rest_framework import serializers
+from djmoney.contrib.django_rest_framework import MoneyField
 from .models import Account
+from .exceptions import ConflictError
 
 
 class AccountsSerializer(serializers.ModelSerializer):
+    saldo = MoneyField(max_digits=10, decimal_places=2)
+    limite_saque_diario = MoneyField(max_digits=10, decimal_places=2)
+
     class Meta:
-        model=Account
-        fields=["id_pessoa", "saldo", "limite_saque_diario", "flag_ativo", "tipo_conta", "data_criacao"]
-        read_only_fields=["id_pessoa", "data_criacao"]
-    
+        model = Account
+        fields = [
+            "id_pessoa",
+            "saldo",
+            "limite_saque_diario",
+            "flag_ativo",
+            "tipo_conta",
+            "data_criacao",
+        ]
+        read_only_fields = ["data_criacao"]
+
     def create(self, validated_data):
-        last_person = Account.objects.order_by("id_pessoa").last()
-        if not last_person:
-            validated_data["id_pessoa"] = 1
-        else:
-            validated_data["id_pessoa"] = last_person.id_pessoa + 1
-        return super().create(validated_data)
+        id_pessoa = validated_data["id_pessoa"]
+        obj, created = Account.objects.get_or_create(
+            defaults=validated_data, id_pessoa=id_pessoa
+        )
+        if not created:
+            raise ConflictError({"id_pessoa": ["This id_person already exists."]})
+        return obj
+
+
+class AccountsDetailSerializer(serializers.ModelSerializer):
+    saldo = MoneyField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        model = Account
+        fields = ["saldo"]
