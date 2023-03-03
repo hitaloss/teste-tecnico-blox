10e0c5c22065f222706bfc2363111bc65a793c01
hitalo <hitalo.2014.hs@gmail.com>
2023-03-03 16:55:58 -0300

    feat: custom permissions setup

--- transactions/permissions.py
+++ transactions/permissions.py
@@ -1,9 +1,16 @@
+import ipdb
+
 from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
+from django.core.exceptions import ObjectDoesNotExist
+from accounts.models import Account
 
 
-# class AccountDeactivated(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         if view.get_object().flag_ativo:
-#             return True
-#         raise PermissionDenied("You account must be active for this request.")
+class AccountDeactivated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        account_id = view.kwargs.get("pk")
+        account = Account.objects.get(id=account_id)
+        ipdb.set_trace()
+        if account.flag_ativo:
+            return True
+        raise PermissionDenied("You account must be active for this request.")
