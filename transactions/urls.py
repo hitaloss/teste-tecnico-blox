c66b9627eb874fa3ea55782306a0fa6182f6b397
hitalo <hitalo.2014.hs@gmail.com>
2023-03-04 03:27:28 -0300

    feat: added url patterns for transactions

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path("account/<int:pk>/transactions/", views.TransactionListView.as_view()),
     path("account/<int:pk>/deposit/", views.DepositView.as_view()),
     path("account/<int:pk>/withdraw/", views.WithdrawView.as_view()),
 ]
